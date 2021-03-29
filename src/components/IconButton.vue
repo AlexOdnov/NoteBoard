44d46d3bcdd398df14e37c8a52a78debc372f314
AlexOdnov <alex.odnoralov@yandex.ru>
2021-03-30 00:20:44 +0300

    add icon-button component

---
+++ src/components/IconButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button
+    type="button"
+    class="button"
+    :class="[color, size, { 'is-rounded': isRounded }]"
+  >
+    <span class="icon">
+      <font-awesome-icon class="fa-w-12" :icon="type" :size="iconSize" />
+    </span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'IconButton',
+  props: {
+    type: {
+      type: String,
+      required: true,
+    },
+    color: String,
+    size: String,
+    iconSize: String,
+    isRounded: Boolean,
+  },
+};
+</script>
+
+<style scoped>
+.button,
+.button.is-rounded {
+  padding: 1em;
+}
+</style>
