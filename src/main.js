e543addd32e26c56c2512514e095816bc16de625
AlexOdnov <alex.odnoralov@yandex.ru>
2021-03-29 21:51:32 +0300

    Base setup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import 'bulma/css/bulma.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-createApp(App).use(store).use(router).mount("#app");
+import {
+  faPlus,
+  faTimes,
+  faChevronUp,
+  faPen,
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(faPlus, faTimes, faChevronUp, faPen);
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .component('FontAwesomeIcon', FontAwesomeIcon)
+  .mount('#app');
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,22 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/edit',
+    name: 'Edit',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ '../views/Edit.vue'),
   },
+  { path: '/:notFound(.*)', redirect: '/' },
 ];
 
 const router = createRouter({
